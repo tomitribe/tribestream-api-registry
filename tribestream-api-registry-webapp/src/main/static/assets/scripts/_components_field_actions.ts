15475349a6b64e6a3a9412e16e97d709e47e6989
Thiago Veronezi <thiago@veronezi.org>
2016-09-06 09:14:59 -0400

    REG-121 First iteration - refactoring component

---
+++ tribestream-api-registry-webapp/src/main/static/assets/scripts/_components_field_actions.ts
@@ -0,0 +1,46 @@
+///<reference path="../../bower_components/DefinitelyTyped/angularjs/angular.d.ts"/>
+///<reference path="../../bower_components/DefinitelyTyped/underscore/underscore.d.ts"/>
+
+module basecomponents {
+
+    angular.module('website-components-field-actions', [])
+
+        .directive('tribeFieldActions', ['$window', '$document', ($window, $document) => {
+            return {
+                restrict: 'A',
+                scope: {
+                    onCancel: '&',
+                    onConfirm: '&',
+                    active: '=',
+                    version: '='
+                },
+                templateUrl: 'app/templates/component_field_actions.html',
+                controller: ['$scope', ($scope) => {
+                    $scope.confirm = () => $scope.onConfirm();
+                    $scope.cancel = () => $scope.onCancel();
+                }],
+                link: (scope, element, attrs, controller) => {
+                    let floatingBody = angular.element(element.find('> div'));
+                    floatingBody.detach();
+                    var body = $document.find('body');
+                    let adjustOffset = () => {
+                        let position = element.offset();
+                        floatingBody.offset(position);
+                    };
+                    scope.$watch('active', () => {
+                        if (scope.active) {
+                            body.append(floatingBody);
+                            adjustOffset();
+                        } else {
+                            floatingBody.detach();
+                        }
+                    });
+                    scope.$watch('version', () => adjustOffset());
+                    var eWin = angular.element($window);
+                    eWin.bind('resize', adjustOffset);
+                    scope.$on('$destroy', () => eWin.unbind('resize', adjustOffset));
+                }
+            };
+        }]);
+}
+
