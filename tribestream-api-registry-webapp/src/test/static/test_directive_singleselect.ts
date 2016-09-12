c6589a8d9bd768822fb33f3fd6d4715f2044a889
Thiago Veronezi <thiago@veronezi.org>
2016-09-12 16:39:43 -0400

    REG-120 no unit tests - should cancel edit

--- tribestream-api-registry-webapp/src/test/static/test_directive_singleselect.ts
+++ tribestream-api-registry-webapp/src/test/static/test_directive_singleselect.ts
@@ -121,4 +121,34 @@ describe('it tests our custom singleselect component', () => {
         });
     });
 
+    it('should cancel edit', (done) => {
+        timeoutTryCatch(100, done, () => {
+            let scope = rootScope.$new();
+            scope.selected = 'aaa';
+            scope.options = ['aaa', 'bbb', 'ccc', 'ddd', 'eee'];
+            let element = angular.element('<div data-tribe-singleselect data-selected-option="selected" data-available-options="options"></div>');
+            // append to body so we can click on it.
+            element.appendTo(document.find('body'));
+            compile(element)(scope);
+            timeoutTryCatch(100, done, () => {
+                let selected = angular.element(element.find('div[data-tribe-singleselect-selected]'));
+                let input = angular.element(selected.find('input'));
+                timeoutTryCatch(100, done, () => {
+                    input.focus();
+                    let selectedScope = selected.scope();
+                    selectedScope.$apply(() => selectedScope.inputText = 'fff');
+                    timeoutTryCatch(100, done, () => {
+                        triggerKeyDown(input, 27); // escape
+                        timeoutTryCatch(100, done, () => {
+                            timeoutTryCatch(100, done, () => {
+                                expect(selectedScope.selectedItem).to.equal('aaa');
+                                done();
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+
 });
