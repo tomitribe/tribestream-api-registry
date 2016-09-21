1b7c86141be371761e57a67bc90e376efeabc560
Thiago Veronezi <thiago@veronezi.org>
2016-09-21 14:45:13 -0400

    REG-118 typescript use in component filters

--- tribestream-api-registry-webapp/src/main/static/assets/scripts/_components_filters.ts
+++ tribestream-api-registry-webapp/src/main/static/assets/scripts/_components_filters.ts
@@ -1,34 +1,23 @@
-angular.module('website-components-filters', [
+angular.module('website-components-filters', [])
+    
+    .filter('uriencode', ['$window', ($window) => $window.encodeURIComponent])
 
-])
-    .filter('uriencode', ['$window', function ($window) {
-        return $window.encodeURIComponent;
+    .filter('pathencode', [() => (input) => {
+        // The root path is the most simple case
+        if (input === '/') {
+            return '/';
+        }
+        return input.split('/')
+            .map((part) => {
+                return part.match('\{.*\}') ? ':' + part.slice(1, -1) : part
+            })
+            .join('/');
     }])
 
-    .filter('pathencode', [function () {
-        return function (input) {
-            // The root path is the most simple case
-            if (input === '/') {
-                return '/';
-            }
-            return input.split('/')
-                .map((part) => {
-                    return part.match('\{.*\}') ? ':' + part.slice(1, -1) : part
-                })
-                .join('/');
-        }
-    }])
+    .filter('tribeHtml', ['$sce', ($sce) => (input) => $sce.trustAsHtml(input)])
 
-    .filter('tribeHtml', ['$sce', function ($sce) {
-        return function (input) {
-            return $sce.trustAsHtml(input);
-        }
-    }])
-
-    .filter('tribeHtmlText', [function () {
-        return function (input) {
-            var el = angular.element('<div></div>');
-            el.append(input);
-            return el.text();
-        }
+    .filter('tribeHtmlText', [() => (input) => {
+        var el = angular.element('<div></div>');
+        el.append(input);
+        return el.text();
     }]);
