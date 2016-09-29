da62cdbc5222e36c73a845673b6fe9cd01334d81
Thiago Veronezi <thiago@veronezi.org>
2016-09-28 22:02:22 -0400

    REG-204 adding slash in case base path is not root

--- tribestream-api-registry-webapp/src/test/static/test_app_endpoint_details.ts
+++ tribestream-api-registry-webapp/src/test/static/test_app_endpoint_details.ts
@@ -5,18 +5,25 @@ describe('it tests the endpoint details page', () => {
     var rootScope;
     var timeout;
     var document;
+    var appEndpointsDetailsHeaderService;
 
     beforeEach(() => angular.module('tribe-endpoints-details'));
+    beforeEach(() => angular.module('website-components-markdown'));
+    beforeEach(() => angular.module('website-components'));
+    beforeEach(() => angular.module('website-components-field-actions'));
+
+
     beforeEach(() => angular.module('website-components'));
 
     beforeEach((done) => {
         angular.injector(['ng', 'tribe-endpoints-details', 'website-components']).invoke([
-            '$compile', '$rootScope', '$timeout', '$document',
-            function ($compile, $rootScope, $timeout, $document) {
+            '$compile', '$rootScope', '$timeout', '$document', 'appEndpointsDetailsHeaderService',
+            function ($compile, $rootScope, $timeout, $document, $appEndpointsDetailsHeaderService) {
                 compile = $compile;
                 rootScope = $rootScope;
                 timeout = $timeout;
                 document = $document;
+                appEndpointsDetailsHeaderService = $appEndpointsDetailsHeaderService;
             }]);
         done();
     });
@@ -31,6 +38,21 @@ describe('it tests the endpoint details page', () => {
             done(e);
         }
     }, ms);
+
+    it('should load baseUrl acording to the swagger file', () => {
+        expect(appEndpointsDetailsHeaderService.getBaseUrl({
+            basePath: '/kakaka',
+            host: 'http://lalala:8080'
+        })).to.equal('http://lalala:8080/kakaka');
+        expect(appEndpointsDetailsHeaderService.getBaseUrl({
+            basePath: '/kakaka/tatata',
+            host: 'http://lalala'
+        })).to.equal('http://lalala/kakaka/tatata');
+        expect(appEndpointsDetailsHeaderService.getBaseUrl({
+            basePath: '/',
+            host: 'http://lalala:8080'
+        })).to.equal('http://lalala:8080');
+    });
 
     it('should load "response request" section', (done) => {
         let scope = rootScope.$new();
@@ -51,7 +73,7 @@ describe('it tests the endpoint details page', () => {
         compile(element)(scope);
         // append to body so we can click on it.
         element.appendTo(document.find('body'));
-        timeoutTryCatch(100, done, () => {
+        timeoutTryCatch(200, done, () => {
             expect(element.html()).to.contain('lalala');
             let compiledScope = element.find('> div').scope();
             compiledScope.addErrorCode();
