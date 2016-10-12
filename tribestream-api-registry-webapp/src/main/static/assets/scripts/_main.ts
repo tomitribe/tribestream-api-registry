6641e87fe0300694758ec51a72328cc283b7c6c7
Thiago Veronezi <thiago@veronezi.org>
2016-10-12 14:11:24 -0400

    Merge branch 'master' into REG-217_remove_ts_warnings
    # Conflicts:
    #	tribestream-api-registry-webapp/src/main/static/assets/scripts/_components.ts
    #	tribestream-api-registry-webapp/src/main/static/assets/scripts/_services_endpoints.ts
    #	tribestream-api-registry-webapp/src/main/static/assets/scripts/endpoints.ts
    #	tribestream-api-registry-webapp/src/main/static/assets/scripts/endpoints_details.ts

--- tribestream-api-registry-webapp/src/main/static/assets/scripts/_main.ts
+++ tribestream-api-registry-webapp/src/main/static/assets/scripts/_main.ts
@@ -17,131 +17,140 @@ import './endpoints_details.ts';
 
 require("../styles/app.sass");
 
-angular.module('tribe-main', [
-    'website-components',
-    'website-services',
-    'website-messages',
-    'ngRoute',
-    'ngStorage',
-    'foundation',
-    'tribe-app',
-    'tribe-authentication',
-    'tribe-endpoints',
-    'tribe-endpoints-details'
-])
+module tribe_main {
 
-    .config([
-        '$locationProvider', '$routeProvider', '$httpProvider',
-        function ($locationProvider, $routeProvider, $httpProvider) {
-            // important so that we can intercept any 401 and fire
-            // an authentication process
-            $httpProvider.interceptors.push('httpInterceptor');
+    // https://webpack.github.io/docs/list-of-plugins.html#defineplugin
+    declare var PRODUCTION: boolean;
 
-            $locationProvider.html5Mode({
-                enabled: true,
-                requireBase: true
-            });
-            $routeProvider
-                .when('/', {
-                    template: require('../templates/page_endpoints.jade')
-                })
-                .when('/showcase', {
-                    template: require('../templates/page_components.jade'),
-                    controller: ['$scope', ($scope) => {
-                        $scope.toUppercase = (item) => {
-                            if (!item) {
-                                return null;
-                            }
-                            if (item.text) {
-                                return item.text.toUpperCase();
-                            }
-                            return item.toUpperCase();
-                        };
-                    }]
-                })
-                .when('/see/:aggregatedId', {
-                    template: require('../templates/page_see.jade'),
-                    controller: ['$scope', '$routeParams', function ($scope, $routeParams) {
-                        $scope.aggregatedId = $routeParams.aggregatedId;
-                    }]
-                })
-                .when('/application/:applicationName*', {
-                    template: require('../templates/page_application_details.jade'),
-                    controller: ['$scope', '$routeParams', function ($scope, $routeParams) {
-                        $scope.app = $routeParams.applicationName;
-                    }]
-                })
-                .when('/application', {
-                    templateUrl: 'app/templates/page_application_details.html',
-                    controller: ['$scope', '$routeParams', function ($scope, $routeParams) {
-                        //$scope.app = $routeParams.applicationName;
-                    }]
-                })
-                .when('/endpoint/:application/:verb/:endpoint*', {
-                    template: require('../templates/page_endpoints_details.jade'),
-                    controller: ['$scope', '$routeParams', function ($scope, $routeParams) {
-                        $scope.requestMetadata = {
-                          applicationName: $routeParams.application,
-                          verb: $routeParams.verb,
-                          endpointPath: $routeParams.endpoint,
-                          version: $routeParams.version
-                        };
-                    }]
-                })
-                .when('/endpoint/:application', {
-                    template: require('../templates/page_endpoints_details.jade'),
-                    controller: ['$scope', '$routeParams', function ($scope, $routeParams) {
-                        $scope.requestMetadata = {
-                          applicationName: $routeParams.application,
-                          version: $routeParams.version
-                        };
-                    }]
-                })
-                .when('/login', {
-                    template: require('../templates/page_login.jade')
-                })
-                .otherwise({
-                    controller: ['$scope', '$location', function ($scope, $location) {
-                        $scope.path = $location.path();
-                    }],
-                    template: require('../templates/page_not_implemented.jade')
-                });
-        }
+    angular.module('tribe-main', [
+        'website-components',
+        'website-services',
+        'website-messages',
+        'ngRoute',
+        'ngStorage',
+        'foundation',
+        'tribe-app',
+        'tribe-authentication',
+        'tribe-endpoints',
+        'tribe-endpoints-details'
     ])
 
-    // should never be used cause we force the user to being logged to use the console
-    .factory('httpInterceptor', ['$q', '$window', '$location', function ($q, $window, $location) {
-        return {
-            'responseError': function (response) {
-                if (response.status === 401) {
-                    $location.url('/login');
-                }
-                return $q.reject(response);
+        .config([
+            '$locationProvider', '$routeProvider', '$httpProvider',
+            function ($locationProvider, $routeProvider, $httpProvider) {
+                // important so that we can intercept any 401 and fire
+                // an authentication process
+                $httpProvider.interceptors.push('httpInterceptor');
+
+                $locationProvider.html5Mode({
+                    enabled: true,
+                    requireBase: true
+                });
+                $routeProvider
+                    .when('/', {
+                        template: require('../templates/page_endpoints.jade')
+                    })
+                    .when('/showcase', {
+                        template: require('../templates/page_components.jade'),
+                        controller: ['$scope', ($scope) => {
+                            $scope.toUppercase = (item) => {
+                                if (!item) {
+                                    return null;
+                                }
+                                if (item.text) {
+                                    return item.text.toUpperCase();
+                                }
+                                return item.toUpperCase();
+                            };
+                        }]
+                    })
+                    .when('/see/:aggregatedId', {
+                        template: require('../templates/page_see.jade'),
+                        controller: ['$scope', '$routeParams', function ($scope, $routeParams) {
+                            $scope.aggregatedId = $routeParams.aggregatedId;
+                        }]
+                    })
+                    .when('/application/:applicationName*', {
+                        template: require('../templates/page_application_details.jade'),
+                        controller: ['$scope', '$routeParams', function ($scope, $routeParams) {
+                            $scope.app = $routeParams.applicationName;
+                        }]
+                    })
+                    .when('/application', {
+                        templateUrl: 'app/templates/page_application_details.html',
+                        controller: ['$scope', '$routeParams', function ($scope, $routeParams) {
+                            //$scope.app = $routeParams.applicationName;
+                        }]
+                    })
+                    .when('/endpoint/:application/:verb/:endpoint*', {
+                        template: require('../templates/page_endpoints_details.jade'),
+                        controller: ['$scope', '$routeParams', function ($scope, $routeParams) {
+                            $scope.requestMetadata = {
+                                applicationName: $routeParams.application,
+                                verb: $routeParams.verb,
+                                endpointPath: $routeParams.endpoint,
+                                version: $routeParams.version
+                            };
+                        }]
+                    })
+                    .when('/endpoint/:application', {
+                        template: require('../templates/page_endpoints_details.jade'),
+                        controller: ['$scope', '$routeParams', function ($scope, $routeParams) {
+                            $scope.requestMetadata = {
+                                applicationName: $routeParams.application,
+                                version: $routeParams.version
+                            };
+                        }]
+                    })
+                    .when('/login', {
+                        template: require('../templates/page_login.jade')
+                    })
+                    .otherwise({
+                        controller: ['$scope', '$location', function ($scope, $location) {
+                            $scope.path = $location.path();
+                        }],
+                        template: require('../templates/page_not_implemented.jade')
+                    });
             }
-        };
-    }])
+        ])
 
-    .run(['$rootScope', function ($rootScope) {
-        $rootScope.baseFullPath = angular.element('head base').first().attr('href');
-    }])
+        // should never be used cause we force the user to being logged to use the console
+        .factory('httpInterceptor', ['$q', '$window', '$location', function ($q, $window, $location) {
+            return {
+                'responseError': function (response) {
+                    if (response.status === 401) {
+                        $location.url('/login');
+                    }
+                    return $q.reject(response);
+                }
+            };
+        }])
 
-    .run(['tribeAuthorizationService', '$sessionStorage', function (Authorization, $sessionStorage) {
-        if ($sessionStorage.tribe == null) {
-            $sessionStorage.tribe = {};
-        } else {
-            Authorization.restoreSession();
-        }
-    }])
+        .run(['$rootScope', function ($rootScope) {
+            $rootScope.baseFullPath = angular.element('head base').first().attr('href');
+        }])
 
-    .run(['tribeAuthorizationService', '$sessionStorage', '$location', function (Authorization, $sessionStorage, $location) {
-        // redirect to login in case the session storage is empty.
-        if (!$sessionStorage.tribe.isConnected) {
-            // save target path to be used once we successfuly log in.
-            Authorization.targetPath = $location.path();
-            $location.path("/login");
-        }
-    }])
+        .run(['tribeAuthorizationService', '$sessionStorage', function (Authorization, $sessionStorage) {
+            if ($sessionStorage.tribe == null) {
+                $sessionStorage.tribe = {};
+            } else {
+                Authorization.restoreSession();
+            }
+        }])
 
-    .run(function () {
-        // placeholder
-    });
+        .run(['tribeAuthorizationService', '$sessionStorage', '$location', function (Authorization, $sessionStorage, $location) {
+            // redirect to login in case the session storage is empty.
+            if (!$sessionStorage.tribe.isConnected) {
+                // save target path to be used once we successfuly log in.
+                Authorization.targetPath = $location.path();
+                $location.path("/login");
+            }
+        }])
+
+        .config(['$logProvider', function ($logProvider) {
+            if (PRODUCTION) {
+                $logProvider.debugEnabled(false);
+            }
+        }]);
+
+}
--- tribestream-api-registry-webapp/src/main/static/webpack.config.js
+++ tribestream-api-registry-webapp/src/main/static/webpack.config.js
@@ -8,10 +8,6 @@ module.exports = webpackMerge(commonConfig, {
     plugins: [
         new webpack.NoErrorsPlugin(),
         new webpack.optimize.DedupePlugin(),
-        //new webpack.optimize.UglifyJsPlugin({ // https://github.com/angular/angular/issues/10618
-        //    mangle: false
-        //}),
-
         new webpack.DefinePlugin({
             PRODUCTION: JSON.stringify(true)
         })
