93ac8127ccf4b17a061c1730e5d63c2c57c9d62f
rmannibucau <rmannibucau@apache.org>
2016-10-14 08:51:26 +0200

    no need of setters/getters with @JsonProperty

--- tribestream-api-registry-webapp/src/main/java/org/tomitribe/tribestream/registryng/security/oauth2/AccessTokenResponse.java
+++ tribestream-api-registry-webapp/src/main/java/org/tomitribe/tribestream/registryng/security/oauth2/AccessTokenResponse.java
@@ -19,9 +19,10 @@
 package org.tomitribe.tribestream.registryng.security.oauth2;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
+@Data
 public class AccessTokenResponse {
-
     @JsonProperty("scope")
     private String scope;
 
@@ -36,65 +37,4 @@ public class AccessTokenResponse {
 
     @JsonProperty("expires_in")
     private int expiresIn = 120;
-
-    @JsonProperty("access_token")
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    @JsonProperty("access_token")
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    @JsonProperty("refresh_token")
-    public String getRefreshToken() {
-        return refreshToken;
-    }
-
-    @JsonProperty("refresh_token")
-    public void setRefreshToken(String refreshToken) {
-        this.refreshToken = refreshToken;
-    }
-
-    @JsonProperty("token_type")
-    public String getTokenType() {
-        return tokenType;
-    }
-
-    @JsonProperty("token_type")
-    public void setTokenType(String tokenType) {
-        this.tokenType = tokenType;
-    }
-
-    @JsonProperty("expires_in")
-    public int getExpiresIn() {
-        return expiresIn;
-    }
-
-    @JsonProperty("expires_in")
-    public void setExpiresIn(int expiresIn) {
-        this.expiresIn = expiresIn;
-    }
-
-    @JsonProperty("scope")
-    public String getScope() {
-        return scope;
-    }
-
-    @JsonProperty("scope")
-    public void setScope(String scope) {
-        this.scope = scope;
-    }
-
-    @Override
-    public String toString() {
-        return "AccessTokenResponse{" +
-                "accessToken='" + accessToken + '\'' +
-                ", refreshToken='" + refreshToken + '\'' +
-                ", tokenType='" + tokenType + '\'' +
-                ", expiresIn=" + expiresIn +
-                ", scope='" + scope + '\'' +
-                '}';
-    }
 }
